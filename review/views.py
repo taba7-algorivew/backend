6f892f1dd7741c39e200bc8551cc977d6143abd3
HyeongSikkk <2674625@naver.com>
2025-02-17 10:29:05 +0900

    code merged & api들 다 구현

--- review/views.py
+++ review/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from datetime import datetime
-from .models import History, Review, Problem
+from .models import History, Review, Problem, Solution
 from user_auth.models import AlgoReviewUser
 from .ai_module import generate_ai_review  # ai_module에서 함수 불러오기
 
@@ -19,10 +19,10 @@ def get_histories(request, user_id) :
         .values("id", "problem_id", "problem_id__name", "name") \
         .order_by("-created_at")
 
-
-    problem_set= set()
-    problem_dict= {}
-    problems= []
+    # 같은 문제 번호를 가진 데이터들을 뭉쳐두기
+    problem_set= set() # 이미 뭉쳐진 번호가 있는지 체크하기 위함
+    problem_dict= {} # 같은 문제 번호를 가진 히스토리를 뭉칠 곳
+    problems= [] # 최종적으로 리턴할 데이터
     for history in histories :
         # 문제 정보
         problem_id= history["problem_id"]
@@ -45,7 +45,7 @@ def get_histories(request, user_id) :
             }
             
             problems.append(problem_dict[problem_id])
-    print({"problems": problems})
+    #print({"problems": problems})
     return Response(
         {"problems": problems}, 
         status=status.HTTP_200_OK,
@@ -55,9 +55,11 @@ def get_histories(request, user_id) :
 @api_view(['GET'])
 def get_history(request, history_id) :
     history= History.objects.filter(id=history_id).first()
+    problem= Problem.objects.filter(id= history.problem_id.id).first()
     reviews= Review.objects.filter(history_id=history_id).values("id", "title", "comments", "start_line_number", "end_line_num")
     return_data= {
-        "problem_info": history.problem_id,
+        "problem_id": problem.id,
+        "problem_info": problem.content,
         "source_code": history.source_code,
         "history_id": history.id,
         "reviews": reviews,
@@ -65,9 +67,7 @@ def get_history(request, history_id) :
     return Response(
         return_data,
         status=status.HTTP_200_OK,
-    )
-            
-            
+    )  
             
   
 @api_view(["POST"])
@@ -108,21 +108,12 @@ def generate_review(request):
     else :
         problem= Problem.objects.filter(id= problem_info)
     
-    # 히스토리 이름 기본값 생성
-    now= datetime.now()
-    default_name= now.strftime("%Y-%m-%d: %H:%M:%S")
-    
-    
-    # 문제 정보와 소스코드로 API에게 리뷰 생성 요청
-
-
-
     if problem_data["status"]:
         prob = f"{problem_data['title']}\n{problem_data['description']}"
     else:
         raise AssertionError
     # code = source_code
-
+    
     #########################################################
     final_list = generate_ai_review(prob, source_code,problem_info)
     #########################################################
@@ -132,7 +123,7 @@ def generate_review(request):
     history= History.objects.create(
         user_id= user,
         problem_id= problem,
-        name= default_name,
+        name= "name",
         type= 1, # api를 통해 파악해야 할 컬럼
         source_code= source_code,
     )
@@ -140,12 +131,13 @@ def generate_review(request):
     # "problem_info" : prob
     return_data = {
         "history_id": history.id,
-        "problem_info": problem.id,
+        "history_name": None, # 리뷰 정제 후 지정
+        "problem_id": problem.id,
+        "problem_info": problem.content,
         "reviews": []
     }
 
-    # review_id는 1부터 시작하여 1씩 증가
-    print(final_list)
+    #print(final_list)
     for review in final_list:
         title= review[0]
         comments= review[1]
@@ -166,7 +158,10 @@ def generate_review(request):
             "end_line_number": review[3]
         }
         return_data["reviews"].append(review_data)
-    
+        # 히스토리 이름 지정
+        history.name= return_data["reviews"][0]["title"] #리뷰의 첫번째 타이틀
+        history.save()
+        return_data["history_name"]= history.name
     return Response(
         return_data, 
         status=status.HTTP_201_CREATED
@@ -195,3 +190,42 @@ def handle_history(request, history_id) :
 @api_view(["PUT", "DELETE"])
 def handle_problem(request, problem_id):
     problem= Problem.objects.filter(id= problem_id).first()
+    
+    if request.method == "PUT" :
+        new_name= request.data.get("new_name")
+        problem.name= new_name
+        problem.save()
+        return Response(status=status.HTTP_200_OK)
+    
+    elif request.method == "DELETE" :
+        history= History.objects.filter(problem_id=problem).update(is_deleted=True)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    else :
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+    
+# 모범 답안 조회
+@api_view(["GET"])
+def get_solution(request, history_id) :
+    solution= Solution.objects.filter(history_id=history_id).first()
+    return_data= {
+        "history_id": history_id,
+        "solution_code": solution.solution_code
+    }
+    return Response(return_data, status=status.HTTP_200_OK)
+
+# chatbot api
+@api_view(["POST"])
+def chatbot(request) :
+    data= request.data
+    answer= data["question"][-1]
+    # 임의의 대답을 생성하기 위한 가짜 코드
+    from random import random
+    rand_num= random()
+    if rand_num < 0.333333333333 :
+        answer= f"'{answer}' 라는 질문은.. 저도 궁금해요.."
+    elif rand_num < 0.66666666666666666 :
+        answer= f"혹시 제게 '{answer}' 라고 물어보셨나요?"
+    else :
+        answer= f"안들린다아아아 안들린다아아아 {answer} 안들린다아아아아"
+    return_data= {"response": answer}
+    return Response(return_data, status=status.HTTP_200_OK)
